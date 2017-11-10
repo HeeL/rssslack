a7013e7c386e4a81351d9a8c89f212ebad213ba4
Sergii Paryzhskyi <sergii.paryzhskyi@holidaycheck.com>
2017-11-10 15:47:25 +0100

    Read last item title from blog rss

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,22 @@
 module Lib
-    ( someFunc
+    ( getRss
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Text.Regex.TDFA ((=~))
+import Data.List (intercalate)
+import Network.Curl      (curlGetString)
+import Network.Curl.Opts
+import Text.Feed.Import (parseFeedString)
+import Text.Feed.Query (getFeedItems, getItemTitle)
+import Codec.Binary.UTF8.String (encodeString)
+
+feedUrl = "http://techblog.holidaycheck.com/feed.xml"
+
+getRss :: IO String
+getRss = do
+    (_,feed) <- curlGetString feedUrl []
+    return $ getMsg $ head $ getItems feed
+    where
+        getItems = maybe (error "rss parsing failed!") getFeedItems . parseFeedString
+        getMsg   = maybe (error "rss-item parsing failed!")  encodeString . getItemTitle
+
